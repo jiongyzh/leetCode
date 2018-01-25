142cf4506c6aad421da54c38d8a2db8a11ee5aa2
Jiongyan Zhang <jiongyzh@gmail.com>
2018-01-25 17:35:27 +1100

    another solusion for longest palindromic string

--- longest palindromic string.py
+++ longest palindromic string.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def longestPalindrome(self, s):
+    def longestPalindrome1(self, s):
         """
         :type s: str
         :rtype: str
@@ -19,8 +19,32 @@ class Solution(object):
                     break
         return result
 
+    def longestPalindrome(self, s):
+        """
+        :param s: str
+        :return: str
+        """
+        start, end = 0, 0
+        for i in xrange(len(s)):
+            len1 = self.expand_from_centre(s, i, i)
+            len2 = self.expand_from_centre(s, i, i+1)
+            max_len = max(len1,len2)
+            if max_len > end - start + 1:
+                start = i - (max(len1,len2)-1)/2
+                end = i + max(len1,len2)/2
+        return s[start: end + 1]
+
+
+    @staticmethod
+    def expand_from_centre(s, left, right):
+        while(left >= 0 and right < len(s) and s[left] == s[right]):
+            left, right = left - 1, right + 1
+        return right - left - 1
+
+
+
 if __name__ == '__main__':
-        print Solution().longestPalindrome('aaaabcdcbasss')
+        print Solution().longestPalindrome('daadb')
 
 
 
