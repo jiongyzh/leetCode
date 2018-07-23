90770f3b37636903803191f21e714d1c83fa539a
aaronzh <aaron@burnsred.com.au>
2018-07-23 16:11:19 +1000

    a second method

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -25,6 +25,39 @@ class Solution(object):
             lc.next = ListNode(1)
         return lr.next
 
+    def addTwoNumbers2(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        l3 = l1
+        p1 = l1
+        while l1 and l2:
+            carry1 = carry
+            carry = (l1.val + l2.val + carry) / 10
+            l1.val = (l1.val+l2.val+carry1) % 10
+            p1 = l1
+            l1 = l1.next
+            l2 = l2.next
+        if l2:
+            p1.next = l2
+            l1 = l2
+        while l1:
+            if l1.val + carry < 10:
+                l1.val = l1.val + carry
+                carry = 0
+                break
+            carry1 = carry
+            carry = (l1.val+carry) / 10
+            l1.val = (l1.val+carry1) % 10
+            p1 = l1
+            l1 = l1.next
+        if carry > 0:
+            p1.next = ListNode(carry)
+        return l3
+
 if __name__ == '__main__':
     a = ListNode(1)
     a.next = ListNode(2)
@@ -32,8 +65,9 @@ if __name__ == '__main__':
     b = ListNode(7)
     b.next = ListNode(8)
     b.next.next = ListNode(9)
-    r = Solution().addTwoNumbers(a, b)
+    b.next.next.next = ListNode(5)
+    b.next.next.next.next = ListNode(4)
+    r = Solution().addTwoNumbers2(a, b)
     while r:
         print r.val
         r = r.next
-
