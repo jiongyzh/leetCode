cefed5adc15d0a8377268864727cfd7da8c250b0
aaronzh <aaron@burnsred.com.au>
2018-07-25 11:23:53 +1000

    a second solution

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -11,6 +11,41 @@ class Solution(object):
             j += 1
         return max_len
 
+    def lengthOfLongestSubstring2(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        r, f, i, m = 0, 0, -1, {}
+        for v in s:
+            i = i+1
+            if v in m and m[v] >= f:
+                if r < i-f:
+                    r = i-f
+                f = m[v] + 1
+            m[v] = i
+        if r < i+1-f:
+            r=i+1-f
+        return r
+
+    def lengthOfLongestSubstring3(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_val, start, char_dict = 0, 0, dict()
+        for i, ch in enumerate(s):
+            if ch in char_dict and char_dict[ch] >= start:  # and :
+                start = char_dict[ch] + 1
+                char_dict[ch] = i
+            else:
+                char_dict[ch] = i
+                val = i - start + 1
+                if max_val < val:
+                    max_val = val
+
+        return max_val
+
 
 if __name__ == '__main__':
-    print Solution().lengthOfLongestSubstring('dvdc')
+    print Solution().lengthOfLongestSubstring3('efdgdfavdc')
