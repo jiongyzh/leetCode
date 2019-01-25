c3ada7f1cb5d1a4146981db62eefc2e5ce1a5360
aaronzh <aaron@burnsred.com.au>
2019-01-25 14:55:23 +1100

    palindrome_number1

--- Substring-with-Concatenation-of-All-Words.py
+++ Substring-with-Concatenation-of-All-Words.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def findSubstring(self, s, words):
+    def findSubstring0(self, s, words):
         """
         :type s: str
         :type words: List[str]
@@ -10,7 +10,7 @@ class Solution(object):
         m, n, o, res = len(s), len(words), len(words[0]), []
         if m < n * o:
             return []
-        for i in range(m-n*o + 1):
+        for i in range(m-n*o+1):
             target = words[:]
             j = i
             while target:
@@ -22,6 +22,34 @@ class Solution(object):
             if not target:
                 res.append(i)
         return res
+
+    def findSubstring(self, s, words):
+        if not words:
+            return []
+        m, n, o, res = len(s), len(words), len(words[0]), []
+        if m < n * o:
+            return []
+        w_dst = list(set(words))
+        w_cnt = list(words.count(x) for x in w_dst)
+        w_dic = dict(zip(w_dst, w_cnt))
+        print w_dic
+
+        for i in range(m-n*o+1):
+            target = w_dic.copy()
+            j = i
+            while j < i + n*o:
+                try:
+                    if target[s[j:j+o]] > 0:
+                        target[s[j:j + o]] -= 1
+                        j += o
+                    else:
+                        break
+                except KeyError:
+                    break
+            if j == i + n*o:
+                res.append(i)
+        return res
+
 
 if __name__ == '__main__':
     print Solution().findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"])
